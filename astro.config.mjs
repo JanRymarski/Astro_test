fef080092fb480f08812463a409150c7bd650a8e
JanRymarski <jan.rymarski@student.howest.be>
2024-11-14 16:19:01 +0100

    Finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-    site: "https://JanAstroTest.com"
-  });
+  site: "https://JanAstroTest.com",
+  integrations: [preact()]
+});
